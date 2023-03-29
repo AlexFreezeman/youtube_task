f6b9d88589c0ca1509840609d5f7cd994a474350
AlexFreezeman <winterirk@gmail.com>
2023-03-29 21:08:03 +0800

    task 2 (готово?)

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from utils.channel import Channel
 
 def main():
 
-    channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'  # вДудь
+#    channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'  # вДудь
     # channel_id = 'UC1eFXmJNkjITxPFWTy6RsWg'    # Редакция
 
-    channel = Channel(channel_id)
-    channel.print_info()
+#    channel = Channel(channel_id)
+#    channel.print_info()
 
     vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
 #    vdud.print_info()
@@ -15,7 +15,7 @@ def main():
     print(vdud.video_count)
     print(vdud.url)
 
-    # менять не можем --- на самом деле можем, как починить? добавляется в json в конце
+    # менять не можем
     vdud.channel_id = 'Новое название'
     print(vdud.channel_id)
 
--- utils/channel.py
+++ utils/channel.py
@@ -9,13 +9,13 @@ class Channel:
     # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
     api_key: str = os.getenv('youtube')
 
-    def __init__(self, id):
-        self.__id = id
+    def __init__(self, channel_id):
+        self.__channel_id = channel_id
         self.json = ""
         self.get_json()
         self.title = self.json["items"][0]['snippet']['title']
         self.channel_description = self.json["items"][0]['snippet']['description']
-        self.url = r"https://www.youtube.com/channel/" + self.__id
+        self.url = r"https://www.youtube.com/channel/" + self.__channel_id
         self.video_count = self.json["items"][0]["statistics"]["videoCount"]
         self.channel_number_of_views = self.json["items"][0]["statistics"]["viewCount"]
 
@@ -24,7 +24,7 @@ class Channel:
         #        with youtube_api as youtube:
         #            channel = youtube.channels().list(id=self.id, part='snippet,statistics').execute()
         #            self.json = json.dumps(channel, indent=2, ensure_ascii=False)
-        channel = self.get_service().channels().list(id=self.__id, part="snippet,statistics").execute()
+        channel = self.get_service().channels().list(id=self.__channel_id, part="snippet,statistics").execute()
         self.json = channel
 
     def save_json(self, path):
@@ -44,3 +44,13 @@ class Channel:
         #        youtube_api = build('youtube', 'v3', developerKey=os.getenv('youtube'))
         with build('youtube', 'v3', developerKey=cls.api_key) as youtube_api:
             return youtube_api
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @channel_id.setter
+    def channel_id(self, channel_id):
+        if self.channel_id != channel_id:
+            raise AttributeError(f"property 'channel_id' of 'Channel' object has no setter")
+        self.__channel_id = channel_id
